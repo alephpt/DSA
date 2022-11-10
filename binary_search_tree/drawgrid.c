9fa2e66fe622090cb805d2a1e8e4cc62b5d26ed6
alephpt <alephpt1@gmail.com>
2022-11-10 14:08:37 -0600

    Proper Tree Rendering

--- binary_search_tree/drawgrid.c
+++ binary_search_tree/drawgrid.c
@@ -1,49 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
-int layers = 7;
-int layer_cnt = 1;
-int horiz_center = 0;
-int verti_center = 0;
-int sub_count = 0;
-
-int layer_offset() {
-    if (layer_cnt == 1) {
-        return layer_cnt;
-    }
-    return 
-}
+const int layers = 5;                                           // number of layers
+const int canvas = layers * 7;                                          // size of canvas
+const int h = canvas;                                           // pass canvas to height
+const int w = (int)(canvas * 2.3);                              // scale canvas to width for ascii scaling
 
 void print_canvas(int width, int height) {
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            if ((x == width / 2) || 
-                (y == height / 2)) {
-                printf("x");
-            } else {
-                printf(".");
+    int h_offset = 0;
+    int sub_x_count = 0;
+    int sub_y_count = 0;
+    int branches = 0;
+    int branch_offset = 0;
+    
+    for (int y = layers; y < height; y++)                            // for each row
+    {
+        int v_offset = (int)((h / layers) / (sub_y_count + 1));     // vertical offset is the height / layers / sub_y
+        int y_row_offset = y + (int)(v_offset * 0.5);               // row offset is half the vertical offset
+        sub_x_count = 0;                                            // reset x
+
+        for (int x = 0; x < width; x++)                             // for each column
+        {
+            if (sub_y_count < layers &&                             // if the y count < total layers
+                y_row_offset % v_offset == 0)                       // and if the row - 1/2 offset % total offset
+            {                           
+                if(x == width / 2 &&                                // if x is 1/2 width &&
+                   sub_y_count == 0)                                // we're on the first row
+                {                                                                  
+                    printf("%d", sub_y_count);                      // print the number in the middle
+                } else 
+                if (sub_x_count < branches &&                       // if the subdivision counter is less than the branches
+                    (x - branch_offset) % h_offset == 0)            // and the x less the offset % the branches subdivisions = 0
+                {                                   
+                    printf("%d", sub_y_count);                      // print the number
+                    sub_x_count++;                                  // and increase the subdivisions counter
+                } else 
+                {
+                    printf(" ");
+                }
+            } 
+            else 
+            {
+                printf(" ");
+            }
+        }
+ 
+        if (y_row_offset % v_offset == 0) 
+        {
+            sub_y_count++;
+            branches = pow(2, sub_y_count);
+            h_offset = (int)w /branches;
+            branch_offset = (int)(h_offset * 0.5);
+
+            if (sub_y_count > layers) {
+                return;
             }
         }
         printf("\n");
     }
-
     return;
 }
 
 int main () {
-    int canvas = 25; 
-    int height = canvas; 
-    int width = (int)(canvas * 2.3);
-    horiz_center = width / (layer_cnt * 2);
-    verti_center = width / layers;
-    
-    printf("horizontal: %d\n", horiz_center);
-    printf("vertical: %d\n", verti_center);
-    printf("\n");
-
-    print_canvas(width, height);
+    print_canvas(w, h);
 
     return 0;
 }
-
